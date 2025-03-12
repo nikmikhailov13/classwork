178781002271c293a777660ff6a88d026a4759f8
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-03-12 18:36:11 +0100

    add lombok

--- src/main/java/gmail/nikmikhailov13/lesson16/animals/Cat.java
+++ src/main/java/gmail/nikmikhailov13/lesson16/animals/Cat.java
@@ -1,45 +1,38 @@
 package gmail.nikmikhailov13.lesson16.animals;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cat {
+    public static final boolean HAS_TAIL = true;
     private String name;
     private int age;
     private double weight;
+    private Breed breed;
 
-    public Cat() {
-    }
 
-    public Cat(String name, int age, double weight) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
-    }
+    public void voice() {
+        if (age > 4) {
+        } else {
+            System.out.println("Miau-Miau");
+        }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
     }
 
     @Override
     public String toString() {
-        return "Cat{" + "name='" + name + '\'' + ", age=" + age + ", weight=" + weight + '}';
+        return "Cat{" +
+                "name='" + name +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", breed=" + breed +
+                '}';
     }
+
 }
