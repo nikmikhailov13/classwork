aac8b7dd8b5ccdf546d390f4410707e1f00a443a
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-04-04 18:20:29 +0200

    add exceptions

---
+++ src/main/java/gmail/nikmikhailov13/exceptions/DivisionByZeroException.java
@@ -0,0 +1,8 @@
+package gmail.nikmikhailov13.exceptions;
+
+public class DivisionByZeroException extends Exception {
+
+    public DivisionByZeroException() {
+        super("Division by zero");
+    }
+}
---
+++ src/test/java/gmail/nikmikhailov13/exceptions/CalculatorTest.java
@@ -0,0 +1,33 @@
+package gmail.nikmikhailov13.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    @Test
+    void shouldDoDivision() throws DivisionByZeroException {
+        // given
+        int a = 5;
+        int b = 2;
+
+        // when
+        int result = Calculator.divide(a, b);
+
+        // then
+        assertEquals(2, result);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDivisionByZero() {
+        // given
+        int a = 5;
+        int b = 0;
+
+        // when & then
+        assertThrows(DivisionByZeroException.class, () -> Calculator.divide(a, b));
+    }
+
+}
