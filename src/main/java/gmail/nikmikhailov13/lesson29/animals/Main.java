59e7e9d7d65359b96ab38a960522f72169a0de25
Nikita Mykhailov <nikmikhailov13@gmail.com>
2025-04-10 18:43:34 +0200

    add WordUtils (list, set, map)

--- src/main/java/gmail/nikmikhailov13/lesson29/animals/Main.java
+++ src/main/java/gmail/nikmikhailov13/lesson29/animals/Main.java
@@ -1,20 +1,12 @@
 package gmail.nikmikhailov13.lesson29.animals;
 
-import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        ContactZoo contactZoo = new ContactZoo();
-        for (int i = 0; i < 10; i++) {
-            contactZoo.addAnimal(new Racoon(i + 1, i, "Racoon " + i));
-        }
 
-        System.out.println(contactZoo.getAnimals());
-        Optional<Racoon> optionalRacoon = contactZoo.getRacoonByName("Racoon 5");
-        if (optionalRacoon.isPresent()) {
-            System.out.println(optionalRacoon.get().getName());
-        }
+
+
     }
 
 }
