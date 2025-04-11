912ab9b78388077b168c8f782e8ac8c7b2e09905
Mykola Digtiar <mykola.digtiar@gmail.com>
2025-04-11 19:13:51 +0200

    Lesson 39 (Generics)

---
+++ src/main/java/gmail/nikmikhailov13/lesson39/Box.java
@@ -0,0 +1,23 @@
+package gmail.nikmikhailov13.lesson39;
+
+public class Box<E> {
+    private E element;
+
+    public void add(E e) {
+        element = e;
+    }
+
+    public void remove() {
+        element = null;
+    }
+
+    public E get() {
+        return element;
+    }
+
+    public <S> S doSmth(S smth) {
+        System.out.println(smth.toString());
+
+        return smth;
+    }
+}
