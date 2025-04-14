45f0cec2d448f9fcda2f59a81f28c5cdd754eb9a
Mykola Digtiar <mykola.digtiar@gmail.com>
2025-04-14 18:45:45 +0200

    Lesson 40 (Lambda) (#14)

---
+++ src/main/java/gmail/nikmikhailov13/lesson40/Calculator.java
@@ -0,0 +1,13 @@
+package gmail.nikmikhailov13.lesson40;
+
+public class Calculator implements Operationable<String> {
+
+    @Override
+    public String calculate(String x, String y) {
+        return null;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+}
